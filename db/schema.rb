6e666d9168270f58a76033790e5389a2436618ab
Jeffrey Lui <jlui820@gmail.com>
2020-03-03 11:52:05 -0800

    add fname lname to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_011526) do
+ActiveRecord::Schema.define(version: 2020_03_03_195041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_03_03_011526) do
     t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "firstName"
+    t.string "lastName"
   end
 
 end
