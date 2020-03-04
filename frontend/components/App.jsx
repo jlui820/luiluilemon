e7302759c27598778636e6c09e2ceb8601f52d56
Jeffrey Lui <jlui820@gmail.com>
2020-03-03 22:12:14 -0800

    add session form

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import GreetingContainer from './greeting/greeting_container'
+import GreetingContainer from './greeting/greeting_container';
+import LoginFormContainer from './sessions/login_form_container';
+import SignupFormContainer from './sessions/signup_form_container';
+import { Route } from 'react-router-dom';
+
 
 const App = () => (
     <div>
@@ -7,6 +11,9 @@ const App = () => (
             <h1>LuluLemon</h1>
             <GreetingContainer />
         </header>
+
+        <Route path='/login' component={LoginFormContainer} />
+        <Route path='/signup' component={SignupFormContainer} />
     </div>
 );
 
--- frontend/components/sessions/login_form_container.jsx
+++ frontend/components/sessions/login_form_container.jsx
@@ -3,6 +3,7 @@ import SessionForm from './session_form';
 import { connect } from 'react-redux';
 import { login } from '../../actions/session_actions'
 
+
 const mapStateToProps = ({ errors }) => ({
     errors: errors.session,
     formType: 'login'
