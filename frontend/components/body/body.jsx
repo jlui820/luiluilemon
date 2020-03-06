7976d3e7d870c22ade632f9c0a0a1342de03634e
Jeffrey Lui <jlui820@gmail.com>
2020-03-05 22:51:41 -0800

    change main-body

--- frontend/components/body/body.jsx
+++ frontend/components/body/body.jsx
@@ -8,31 +8,33 @@ class Body extends React.Component {
 
     render() {
         return(
-            <div className='main-body-container'>
-                <div className='main-body-image'>
-                    {/* <img className="main-image" src="/assets/mainpage.jpg" alt="" /> */}
-                        <img className='main-image' src={window.mainpageimage} />
-                </div>
-                <div className='left-side-container'>
-                    <h1>lululemon</h1>
-                    <div className='left-side-container-items'>
-                        <ul className='left-side-container-list'>
-                            <li className='left-side-container-item'>
-                                <Link to="/" className='click-category-link' href="">Women's What's New</Link>
-                            </li>
-                            <li className='left-side-container-item'>
-                                <Link to="/" className='click-category-link' href="">All Women's</Link>
-                            </li>
-                            <li className='left-side-container-item'>
-                                <Link to="/" className='click-category-link' href="">All Men's</Link>
-                            </li>
-                            <li className='left-side-container-item'>
-                                <Link to="/" className='click-category-link' href="">All Girls'</Link>
-                            </li>
-                            <li className='left-side-container-item'>
-                                <Link to="/" className='click-category-link' href="">All Accessories</Link>
-                            </li>
-                        </ul>
+            <div className='lulu-body'>
+                <div className='main-body-container'>
+                    <div className='left-side-container-wrapper'>
+                        <div className='left-side-container'>
+                            <h1 className='left-side-container-title'>lululemon</h1>
+                            <div className='left-side-container-items'>
+                                <ul className='left-side-container-list'>
+                                    <div className='left-side-container-list-wrapper'>
+                                        <li className='left-side-container-item'>
+                                            <Link to="/" className='click-category-link' href="">Women's What's New</Link>
+                                        </li>
+                                        <li className='left-side-container-item'>
+                                            <Link to="/" className='click-category-link' href="">All Women's</Link>
+                                        </li>
+                                        <li className='left-side-container-item'>
+                                            <Link to="/" className='click-category-link' href="">All Men's</Link>
+                                        </li>
+                                        <li className='left-side-container-item'>
+                                            <Link to="/" className='click-category-link' href="">All Girls'</Link>
+                                        </li>
+                                        <li className='left-side-container-item'>
+                                            <Link to="/" className='click-category-link' href="">All Accessories</Link>
+                                        </li>
+                                    </div>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- frontend/components/main_body_products/main_products_body.jsx
+++ frontend/components/main_body_products/main_products_body.jsx
@@ -1,41 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
 
-class MainBodyProducts extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+// class MainBodyProducts extends React.Component {
+//     constructor(props) {
+//         super(props)
+//     }
+// t
+//     render() {
+//         return (
+//             <div className='main-body-products-container'>
+//                 <div className='main-products-container'>
+//                     <div className='main-products-items'>
+//                         <div className='main-product-item'>
+//                             <Link></Link>
+//                             <h3 className='product-description'>ProductDescriptionGoesHere</h3>
+//                             <span className='price'>PriceGoesHere</span>
+//                         </div>
+//                         <div className='main-product-item'>
+//                             <Link></Link>
+//                             <h3 className='product-description'>ProductDescriptionGoesHere</h3>
+//                             <span className='price'>PriceGoesHere</span>
+//                         </div>
+//                         <div className='main-product-item'>
+//                             <Link></Link>
+//                             <h3 className='product-description'>ProductDescriptionGoesHere</h3>
+//                             <span className='price'>PriceGoesHere</span>
+//                         </div>
+//                         <div className='main-product-item'>
+//                             <Link></Link>
+//                             <h3 className='product-description'>ProductDescriptionGoesHere</h3>
+//                             <span className='price'>PriceGoesHere</span>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
-    render() {
-        return (
-            <div className='main-body-products-container'>
-                <div className='main-products-container'>
-                    <div className='main-products-items'>
-                        <div className='main-product-item'>
-                            <Link></Link>
-                            <h3 className='product-description'>ProductDescriptionGoesHere</h3>
-                            <span className='price'>PriceGoesHere</span>
-                        </div>
-                        <div className='main-product-item'>
-                            <Link></Link>
-                            <h3 className='product-description'>ProductDescriptionGoesHere</h3>
-                            <span className='price'>PriceGoesHere</span>
-                        </div>
-                        <div className='main-product-item'>
-                            <Link></Link>
-                            <h3 className='product-description'>ProductDescriptionGoesHere</h3>
-                            <span className='price'>PriceGoesHere</span>
-                        </div>
-                        <div className='main-product-item'>
-                            <Link></Link>
-                            <h3 className='product-description'>ProductDescriptionGoesHere</h3>
-                            <span className='price'>PriceGoesHere</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-}
-
-export default MainBodyProducts;
+// export default MainBodyProducts;
