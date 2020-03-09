145eca9d7e7ad59fe400f32bdc1c8688dd248215
Jeffrey Lui <jlui820@gmail.com>
2020-03-08 21:50:43 -0700

    change main page css

--- frontend/components/body/splashpage.jsx
+++ frontend/components/body/splashpage.jsx
@@ -47,26 +47,70 @@
 // export default Body;
 
 
+// import React from 'react';
+// import { Link } from 'react-router-dom'
+
+// class Body extends React.Component {
+
+//     render() {
+//         return (
+//             <div className='splash-main'>
+//                 <div className='splash-page-main-img'>
+//                     <img className='splash-img' src={window.mainpageimage} alt="" srcset="" />
+//                 </div>
+//                 <div className='left-side-container'>
+//                     <h1 className='left-side-container-title'>lululemon</h1>
+//                     <ul className='left-side-container-categories'>
+//                         <li className='left-side-container-category'>Women's What's New</li>
+//                         <li className='left-side-container-category'>All Women's</li>
+//                         <li className='left-side-container-category'>All Men's</li>
+//                         <li className='left-side-container-category'>All Girl's</li>
+//                         <li className='left-side-container-category'>All Accessories</li>
+//                     </ul>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Body;
 import React from 'react';
 import { Link } from 'react-router-dom'
 
+
 class Body extends React.Component {
+    constructor(props) {
+        super(props) 
+    }
 
     render() {
         return (
-            <div className='splash-main'>
-                <div className='splash-page-main-img'>
-                    <img className='splash-img' src={window.mainpageimage} alt="" srcset="" />
-                </div>
-                <div className='left-side-container'>
-                    <h1 className='left-side-container-title'>lululemon</h1>
-                    <ul className='left-side-container-categories'>
-                        <li className='left-side-container-category'>Women's What's New</li>
-                        <li className='left-side-container-category'>All Women's</li>
-                        <li className='left-side-container-category'>All Men's</li>
-                        <li className='left-side-container-category'>All Girl's</li>
-                        <li className='left-side-container-category'>All Accessories</li>
-                    </ul>
+            <div className='whole-container-wrapper'>
+                <div className='splash-page-main-container'>
+                    <div className='left-container'>
+                        <div className='left-container-title'>lululemon</div>
+                        <ul className='left-container-lists-wrapper'>
+                            <li className='left-container-list-item'>
+                                <Link to="/" className='click-category-link' href="">Women's What's New</Link>
+                            </li>
+                            <li className='left-container-list-item'>
+                                <Link to="/" className='click-category-link' href="">All Women's</Link>
+                            </li>
+                            <li className='left-container-list-item'>
+                                <Link to="/" className='click-category-link' href="">All Men's</Link>
+                            </li>
+                            <li className='left-container-list-item'>
+                                <Link to="/" className='click-category-link' href="">All Girls'</Link>
+                            </li>
+                            <li className='left-container-list-item'>
+                                <Link to="/" className='click-category-link' href="">All Accessories</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='splash-page-main-image-container'>
+                        <img className='splash-page-img' src={window.mainpageimage} alt="" srcset="" />
+                        <div className='image-click-button'></div>
+                    </div>
                 </div>
             </div>
         )
