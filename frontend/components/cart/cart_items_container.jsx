d005f5e503f5668ed08fd6035b2e5cba7c3083dd
Jeffrey Lui <jlui820@gmail.com>
2020-04-01 22:39:25 -0700

    start cart container

---
+++ frontend/components/cart/cart_items_container.jsx
@@ -0,0 +1,13 @@
+import{ connect } from 'react-redux';
+import CartItems from './cart_items';
+import { fetchCartItems, updateCart, removeCartItem} from '../../actions/cart_actions'
+
+const mapStateToProps = state => ({
+
+})
+
+const mapDispatchToProps = dispatch => ({
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartItems);
