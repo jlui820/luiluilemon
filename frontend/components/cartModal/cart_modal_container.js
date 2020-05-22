431c19fa5be79c5e941b6425c3ce638c47181395
Jeffrey Lui <jlui820@gmail.com>
2020-05-22 11:30:57 -0700

    work on modal

--- frontend/components/cartModal/cart_modal_container.js
+++ frontend/components/cartModal/cart_modal_container.js
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import { connect } from 'react-redux';
-// import CartModal from './cartModal'
-
-
-// // const mapStateToProps = state => {
-
-// // }
-
-
-// const mapDispatchToProps = dispatch => ({
-
-// })
-
-
-// export default connect(mapDispatchToProps)(CartModal)
-
-
 
 import React from 'react';
 import { connect } from 'react-redux';
